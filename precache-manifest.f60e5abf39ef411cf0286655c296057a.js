3d3f886db58dd5d943e9bae97a10da42ab2bba3e
fajar13k <fajar.fj13@gmail.com>
2020-05-10 04:45:52 +0700

    Updates

---
+++ precache-manifest.f60e5abf39ef411cf0286655c296057a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "353747ad240d56f7ffb3f8ce3eaead14",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "0f8612d1a75eb188051e",
+    "url": "/portfolio/static/css/main.701bf333.chunk.css"
+  },
+  {
+    "revision": "8dd2b2ba61bbd66b67bc",
+    "url": "/portfolio/static/js/2.bfc9b565.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.bfc9b565.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f8612d1a75eb188051e",
+    "url": "/portfolio/static/js/main.98bc1551.chunk.js"
+  },
+  {
+    "revision": "bb2a8ea6f624a92ea3d4",
+    "url": "/portfolio/static/js/runtime-main.31ed5798.js"
+  },
+  {
+    "revision": "7c590c5d8dd0056e1ec9db9880438546",
+    "url": "/portfolio/static/media/fajar.7c590c5d.jpg"
+  },
+  {
+    "revision": "27ca2945e798d82ba8b66f2f2a1388d0",
+    "url": "/portfolio/static/media/gts-branding.27ca2945.png"
+  },
+  {
+    "revision": "3404396988d50c529967ae88da17a349",
+    "url": "/portfolio/static/media/healthpoint.34043969.png"
+  },
+  {
+    "revision": "0fc2a7fa43337a292b73440d695a93ba",
+    "url": "/portfolio/static/media/kampoeng-mulang-logo.0fc2a7fa.png"
+  },
+  {
+    "revision": "53f7c87748dbe5380e2d25a3b7c8408b",
+    "url": "/portfolio/static/media/smartlibrary.53f7c877.png"
+  }
+]);
